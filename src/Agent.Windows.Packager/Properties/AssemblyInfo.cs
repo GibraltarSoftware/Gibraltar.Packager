d1030f786bb1d61459d79ecbb06ff71d0cb99317
Kendall Miller <kendall@gibraltarsoftware.com>
2019-11-30 18:10:54 -0500

    Update to Gibraltar Agent version 4.8.33

--- src/Agent.Windows.Packager/Properties/AssemblyInfo.cs
+++ src/Agent.Windows.Packager/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gibraltar Software, Inc.")]
 [assembly: AssemblyProduct("Loupe")]
-[assembly: AssemblyCopyright("Copyright © 2008-2018 Gibraltar Software, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2008-2019 Gibraltar Software, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,7 +34,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.7.0.0")]
-[assembly: AssemblyFileVersion("4.7.10.0")]
-[assembly: AssemblyInformationalVersionAttribute("4.7.10.0")]
+[assembly: AssemblyVersion("4.8.0.0")]
+[assembly: AssemblyFileVersion("4.8.33.0")]
+[assembly: AssemblyInformationalVersionAttribute("4.8.33.0")]
 [assembly: AllowPartiallyTrustedCallers]
